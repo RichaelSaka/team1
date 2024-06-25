4cde864055bca409659dd2d98d96cb611a97bcbe
G3trude <135602311+G3trude@users.noreply.github.com>
2024-06-24 18:02:24 -0700

    Update filter.py

--- backend/src/app/filter.py
+++ backend/src/app/filter.py
@@ -1,19 +1,13 @@
 import csv
 
-# takes in user's intended role and industry as well as csv file where stuff is stored
-def job_filter(user_industry, user_role, file_path = job_data.csv):
-    matching_jobs = []
-    # Read the jobs database file
-    with open(file_path, mode='r', newline='', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-
-        # Iterate through each row in the CSV
-        for row in reader:
-            # Check if the row matches the intended role and industry
-            if (user_industry == row['industry']) and (user_role == row['role']):
-                matching_jobs.append(row)
-    # returns the list of roles that match user's information
-    return matching_jobs        
+# takes in user's intended role and industry as well as job industry and industry role
+def job_filter(user_industry, user_role, industry_role, job_industry):
+            # Check if the user's job and industry matches that of the job posted
+            if (user_industry == job_industry) and ( user_role == industry_role):
+                return True
+            else: 
+                return False
+     
     
 
  
